183751e13db42658dacafdc1f47fcea3c90af2c7
Himangshu Mondal <mailtoherock@gmail.com>
2020-09-15 23:59:52 +0530

    added toolbar

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-let lineWidth = 10;
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const brushSize = document.getElementById("brushsize");
+const brushColor = document.getElementById("brushcolor");
+
+let lineWidth = 5;
 let drawIng = false;
+let strokeColor = "black";
+let bgColor = "white";
 
 // resizing canvas
  function resize() {
      canvas.width = window.innerWidth;
      canvas.height = window.innerHeight;
+     ctx.fillStyle = bgColor;
+     ctx.fillRect(0, 0, canvas.width, canvas.height);
  }
 resize();
 window.addEventListener("resize", resize);
@@ -29,13 +38,41 @@ canvas.addEventListener("mousemove", (e) => {
    }
 });
 
+increaseBtn.addEventListener("click", () => {
+    lineWidth += 5;
+
+    if(lineWidth >= 60){
+        lineWidth = 60;
+    }
+    brushsizeUpdate();
+});
+
+decreaseBtn.addEventListener("click", () => {
+    lineWidth -= 5;
+
+    if(lineWidth < 5){
+        lineWidth = 5;
+    }
+    brushsizeUpdate();
+});
+
+brushColor.addEventListener("change", (e)=>{
+    strokeColor = e.target.value;
+});
+
 //  drawing line
 function drawline (x,y) {
     ctx.lineCap = "round";
     ctx.lineWidth = lineWidth;
     ctx.lineTo(x, y);
+    ctx.strokeStyle = strokeColor;
     ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(x, y);
+}
+
+// brush size update
+function brushsizeUpdate() {
+    brushSize.innerHTML = lineWidth;
 }
 
